2c55b4ba9f995244141b8267d8bd71f6d4a2ffb8
URK21CS2010  Joel Renish <109648488+JOEL1238@users.noreply.github.com>
2022-11-02 00:52:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ def partition(arr,low,high):
       if arr[j] <= pivot:
          # increment
          i = i+1
-         arr[i],arr[j] = arr[j],arr[i]
+         arr[i],arr[j] = arr[j],arr[i]  
    arr[i+1],arr[high] = arr[high],arr[i+1]
    return ( i+1 )
 # sort
